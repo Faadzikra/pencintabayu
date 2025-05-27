ae8e687c85cb550b7d01d03500af9768f31e3d07
Faadzikra <faadzikra@gmail.com>
2025-05-27 09:48:05 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,12 +6,12 @@ st.write(
 )
 st.image (""), width=200)
 
-st.title("Aplikasi buronan bandung")
-st.header("Aplikasi mengecek kondisi bayu")
-angka = st.number_input("Angka kesehatan bayu:", value=0, step=1)
+st.title("Aplikasi Sederhana")
+st.header("Aplikasi mengecek nilai Genap/Ganjil")
+angka = st.number_input("Tulis sebuah angka:", value=0, step=1)
 
 if (angka % 2) == 0:
-    st.write(f"{angka} adalah Bilangan stres")
+    st.write(f"{angka} adalah Bilangan Genap")
 else:
-    st.write(f"{angka} Adalah bilangan gila")
+    st.write(f"{angka} Adalah bilangan Ganjil")
     
