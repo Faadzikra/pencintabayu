acb7538f429a3b80cff6e4e9f1debcf88f6e214e
Faadzikra <faadzikra@gmail.com>
2025-05-27 09:43:11 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,14 @@ st.title("Agus kopling")
 st.write(
     "Muka buronan bayu"
 )
+st.image (""), width=200)
+
+st.title("Aplikasi buronan bandung")
+st.header("Aplikasi mengecek kondisi bayu")
+angka = st.number_input("Angka kesehatan bayu:", value=0, step=1)
+
+if (angka % 2) == 0:
+    st.write(f"{angka} adalah Bilangan stres")
+else:
+    st.write(f"{angka} Adalah bilangan gila")
+    
