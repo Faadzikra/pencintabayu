b375c36b110dae2aafb145b9f841b3aa95f1afd3
Faadzikra <faadzikra@gmail.com>
2025-05-27 09:31:42 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("Agus kopling")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Muka buronan bayu"
 )
